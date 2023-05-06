9d4c47dd5eb5d9fb0230715423f94448527d9b60
tonic-6101 <tonicxx341@gmail.com>
2023-05-06 10:19:59 +0200

    added Link

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ import {
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
-// const router = createBrowserRouter([
-//   { path: '/', element: <HomePage /> },
-//   { path: '/products', element: <ProductsPage /> },
-// ]);
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <ProductsPage /> },
+]);
 
-const router = createBrowserRouter(routeDefinitions);
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
