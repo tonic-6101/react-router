60ab925cfca9bc3b67ed38ea9961049dba2865dc
tonic-6101 <tonicxx341@gmail.com>
2023-05-06 07:51:27 +0200

    added router

--- App.js
+++ App.js
@@ -1,5 +1,17 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from './src/pages/Home';
+import ProductsPage from './src/pages/Products';
+
+//https://example.com/
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <ProductsPage /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
